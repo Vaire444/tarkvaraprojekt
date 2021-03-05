826fddd85b010857ab6b31c12b82c2d8ca5b6f7e
unknown <vaire.riiet@gmail.com>
2021-03-05 19:16:45 +0200

    First commit

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -5,8 +5,22 @@ describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
     const msg = "new message";
     const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+      propsData: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
+  });
+
+  it("should add properly", () => {
+    const wrapper = shallowMount(HelloWorld, {});
+    const total = wrapper.vm.add(20, 5);
+    expect(total).toBe(25);
+    expect(total).not.toBe(20);
+    expect(total).not.toBe(5);
+  });
+
+  it("should multiply properly", () => {
+    const wrapper = shallowMount(HelloWorld, {});
+    const total = wrapper.vm.multiply(20, 5);
+    expect(total).toBe(100);
   });
 });
